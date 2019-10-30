e270b197ec822cdc4c56803b54ae58d1ccbf427a
Muktazam Habsi Ashidiqi <hasbeeazam@gmail.com>
2019-10-30 19:44:03 +0700

    pertama commit

---
+++ resources/views/profile/settings.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+            {{-- {{ $user->name() }} --}}
+    </div>
+</div>
+@endsection
